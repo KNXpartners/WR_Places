8a4b9ebfe674a56d12bb5bbeae9e0af21da0b6a6
Alex <7721776@gmal.com>
2022-02-02 14:50:02 +0300

    change empty path in urls.py to manual.html. add paginator to jobsitelist from templates

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .import views
 from django.conf import settings
+from django.views.generic import TemplateView
 
 # app_name = 'Places'
 
@@ -9,15 +10,17 @@ urlpatterns = [
     # path('<int:pk>/', views.PlaceDetailView.as_view(), name ='Place-details'),
     # path('delete/<int:pk>/', views.PlaceDeleteView.as_view(), name = 'Place-delete'),
     # path('update/<int:pk>/', views.PlaceUpdateView.as_view(), name = 'Place-update'),
+    path('', TemplateView.as_view(template_name="manual/manual.html")),
+
+    path('jobsitelist/',
+    views.jobsiteListView.as_view(),
+        name = 'jobsite-list'),
     path('create-jobsite/',
         views.jobsiteCreateView.as_view(),
         name = 'create-jobsite'),
     path('jobsite-structure/',
         views.JobsiteStructure.as_view(),
         name = 'jobsite-structure'),
-    path('',
-        views.jobsiteListView.as_view(),
-        name = 'jobsite-list'),
     path('<int:place_pk>/',
         views.placeView.as_view(),
         name = 'place-view'),
